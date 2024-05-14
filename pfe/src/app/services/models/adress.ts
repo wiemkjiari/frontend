41d72cf19a982bd819ce80cc3c1d134e170ea409
wiemkjiari <88428204+wiemkjiari@users.noreply.github.com>
2024-05-14 09:57:18 +0200

    Delete pfe directory

--- pfe/src/app/services/models/adress.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export interface Adress {
-  adresse1?: string;
-  adresse2?: string;
-  codePostale?: string;
-  pays?: string;
-  ville?: string;
-}
--- pfe/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser'; // Importez bootstrapApplication depuis @angular/platform-browser
-import { appConfig } from './app/app.config'; // Importez votre configuration d'application depuis votre fichier app.config
-import { AppComponent } from './app/app.component'; // Importez votre composant racine AppComponent
-
-bootstrapApplication(AppComponent, appConfig) // Démarrez l'application en passant AppComponent et la configuration de l'application
-    .catch((err) => console.error(err)); 
